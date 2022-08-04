bd79c474cd9fcb17a0672ed7e15e19e21323c9d4
Johannes Maier <johannes.maier@active-group.de>
2022-08-04 16:37:21 +0200

    Try walking through a string/u8 array in Zig

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,31 @@
 const std = @import("std");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    std.log.info("foo", .{});
+    try lex("echo");
+}
+
+const TokenTag = enum { and_if, word };
+
+const Token = union(TokenTag) {
+    and_if: void,
+    word: []const u8,
+};
+
+fn lex(input: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    std.log.info("{s}", .{input});
+
+    var tokens = std.ArrayList(Token).init(allocator);
+    for (input) |_| {
+        try tokens.append(Token{ .word = "abc" });
+    }
+
+    for (tokens.items) |t| {
+        std.log.info("Token: {}", .{t});
+    }
 }
 
 test "basic test" {
