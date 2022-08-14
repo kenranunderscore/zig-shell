efb23cb08f1f8fa745d7cf0afd2e8adf2a124622
Johannes Maier <johannes.maier@mailbox.org>
2022-08-14 11:35:33 +0200

    Lex single quotes and some simple symbols
    First "real" steps with Zig.  Feels great so far, if a little on the
    verbose side in the tests :)

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,131 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("foo", .{});
-    try lex("echo");
-}
-
-const TokenTag = enum { and_if, word };
-
-const Token = union(TokenTag) {
-    and_if: void,
-    word: []const u8,
+const TokenKind = enum {
+    less,
+    lessgreat,
+    dless,
+    dlessdash,
+    greater,
+    dgreater,
+    lparen,
+    rparen,
+    single_quoted,
 };
 
-fn lex(input: []const u8) !void {
+const Token = union(TokenKind) {
+    less: void,
+    lessgreat: void,
+    dless: void,
+    dlessdash: void,
+    greater: void,
+    dgreater: void,
+    lparen: void,
+    rparen: void,
+    single_quoted: []const u8,
+};
+
+const Lexer = struct {
+    input: []const u8,
+    index: u32,
+
+    pub fn init(input: []const u8) Lexer {
+        return Lexer{
+            .input = input,
+            .index = 0,
+        };
+    }
+
+    fn current_char(self: Lexer) !u8 {
+        if (self.index >= self.input.len)
+            return error.EndOfInput;
+        return self.input[self.index];
+    }
+
+    fn advance(self: *Lexer) void {
+        self.index += 1;
+    }
+
+    fn read_single_quoted(self: *Lexer) !Token {
+        // consume leading '
+        self.advance();
+
+        const start_index = self.index;
+        while ((try self.current_char()) != '\'') : (self.advance()) {}
+        const end_index = self.index;
+
+        // advance once more to consume the closing '
+        self.advance();
+
+        return Token{ .single_quoted = self.input[start_index..end_index] };
+    }
+
+    fn read_less(self: *Lexer) Token {
+        self.advance();
+
+        if (self.index < self.input.len and (self.current_char() catch unreachable) == '<') {
+            self.advance();
+            return .dless;
+        } else {
+            return .less;
+        }
+    }
+
+    pub fn next(self: *Lexer) !?Token {
+        const c = self.current_char() catch return null;
+        return switch (c) {
+            '(' => .lparen,
+            ')' => .rparen,
+            '<' => self.read_less(),
+            '\'' => try self.read_single_quoted(),
+            else => unreachable,
+        };
+    }
+
+    pub fn all_tokens(self: *Lexer, allocator: std.mem.Allocator) !std.ArrayList(Token) {
+        var tokens = std.ArrayList(Token).init(allocator);
+        while (true) {
+            const t = try self.next();
+            if (t != null) {
+                try tokens.append(t.?);
+            } else break;
+        }
+
+        return tokens;
+    }
+};
+
+pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-    std.log.info("{s}", .{input});
 
-    var tokens = std.ArrayList(Token).init(allocator);
-    for (input) |_| {
-        try tokens.append(Token{ .word = "abc" });
-    }
+    var lexer = Lexer.init("'a'<<''");
+    var x = lexer.all_tokens(allocator);
+    std.log.info("result: {any}", .{x});
+}
 
-    for (tokens.items) |t| {
-        std.log.info("Token: {}", .{t});
+const expect = std.testing.expect;
+
+test "less" {
+    var lexer = Lexer.init("<");
+    const token = try lexer.next();
+    try expect(token.? == .less);
+}
+
+test "empty single quotes" {
+    var lexer = Lexer.init("''");
+    const token = try lexer.next();
+    switch (token.?) {
+        .single_quoted => |str| try expect(std.mem.eql(u8, str, "")),
+        else => unreachable,
     }
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+test "single quotes with content" {
+    var lexer = Lexer.init("'foo bar'");
+    const token = try lexer.next();
+    switch (token.?) {
+        .single_quoted => |str| try expect(std.mem.eql(u8, str, "foo bar")),
+        else => unreachable,
+    }
 }
